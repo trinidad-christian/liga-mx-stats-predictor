88f330d398b8605607b01ff4bff39897c5f55d43
Christian Trinidad <trinidadchristian560@gmail.com>
2025-07-30 14:14:09 -0500

    Initial project setup

---
+++ src/fetch_data.py
@@ -0,0 +1,20 @@
+# src/fetch_data.py
+import asyncio
+import json
+from sofascore_wrapper.api import SofascoreAPI
+from sofascore_wrapper.search import Search
+
+async def get_player_stats_by_name(name: str):
+    api = SofascoreAPI()
+    search = Search(api, search_string=name)
+    results = await search.search_all()
+    await api.close()
+    return results
+
+if __name__ == "__main__":
+    async def main():
+        data = await get_player_stats_by_name("Gignac")  # You can try Chicharito, Quinones, etc.
+        print(json.dumps(data, indent=4))
+
+    asyncio.run(main())
+# To run this script, use the command: python src/fetch_data.py
